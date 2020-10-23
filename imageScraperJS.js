f48d5c309eaf754fa0dd6bd0b44f48b14d899892
jimmyxee <73322267+jimmyxee@users.noreply.github.com>
2020-10-23 09:59:16 +0800

    Update imageScraperJS.js

--- imageScraperJS.js
+++ imageScraperJS.js
@@ -3,3 +3,14 @@ var srcList = [];
 for(var i = 0; i < images.length; i++) {
     srcList.push(images[i].src);
 }
+
+// OR using the method.
+
+function allSrc() {
+  var src = [];
+  var imgs = document.images;
+  for (var i=0, iLen=imgs.length; i<iLen; i++) {
+    src[i] = imgs[i].src;
+  }
+  return src;
+}
